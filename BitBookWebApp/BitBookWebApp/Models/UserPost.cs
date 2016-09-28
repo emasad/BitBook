0073e7328d0fabdfb26439a0438e15307f55b0ec
emasad <e.md.asad@gmail.com>
2016-09-28 10:22:27 +0600

    Add UserPost field.

--- BitBookWebApp/BitBookWebApp/Models/UserPost.cs
+++ BitBookWebApp/BitBookWebApp/Models/UserPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace BitBookWebApp.Models
 {
     public class UserPost
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public string  PostText { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CurrretTime { get; set; }
     }
 }
