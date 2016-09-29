994b86cc3ab70bb8b73ed60391a73eae28c312d3
emasad <e.md.asad@gmail.com>
2016-09-29 08:18:35 +0600

    Route Name change.

--- BitBookWebApp/BitBookWebApp/App_Start/RouteConfig.cs
+++ BitBookWebApp/BitBookWebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BitBookWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Registration", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
