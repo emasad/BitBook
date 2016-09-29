78ed3ecf08755818c934b533a85fbe2a8d71ed58
Asadullah Sikdar <e.md.asad@gmail.com>
2016-09-29 22:46:28 +0600

    UserFriend.cs file added in Main Project

---
+++ BitBookWebApp/BitBookWebApp/UserFriend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BitBookWebApp.Models
+{
+    public class UserFriend : IEnumerable
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+        public int Friendstatus { get; set; }
+
+        //this is not added
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
